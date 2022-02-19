85ce6505328fe6876ac73f376a3af2ae3909d578
Godwin-Softwares <godwin.softwares@gmail.com>
2022-02-19 15:30:05 +0100

    first update

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import './footer.css'
+import gpt3Logo from '../../assets/logo.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='gpt3__footer section__padding'>
+        <div className='gpt3__footer-heading'>
+           <h1 className='gradient__text'>Do you want to step into the future before others</h1>
+        </div>
+        <div className='gpt3__footer-btn'>
+          <p>Request Early Access</p>
+        </div>
+        <div className='gpt3__footer-links'>
+          <div className='gpt3__footer-links_logo'>
+            <img src={gpt3Logo} alt="Logo"/>
+            <p>Crechterwoord K12 184 DK Alkinjkcv, All</p>
+          </div>  
+          <div className='gpt3__footer-links_div'>
+                <h4>Links</h4>
+                <p>Overons</p>
+                <p>Social Media</p>
+                <p>Counters</p>
+                <p>Contact</p>
+
+          </div>
+
+          <div className='gpt3__footer-links_div'>
+                <h4>Company</h4>
+                <p>Terms & Conditions</p>
+                <p>Privacy Policy</p>
+                <p>Counters</p>
+                <p>Contact</p>
+
+          </div>
+          <div className='gpt3__footer-links_div'>
+                <h4>Get In touch</h4>
+                <p>Crechterwoord K12 184 DK Alkinjkcv</p>
+                <p>08139444402</p>
+                <p>info@paymenow.net</p>
+
+          </div>
+          <div className='gpt3__footer-copyright'>&copy; 2022 GPT-3. All rights reserved</div>
+        </div>
+      </div>
+    
   )
 }
 
--- src/containers/whatGPT3/whatGPT3.jsx
+++ src/containers/whatGPT3/whatGPT3.jsx
@@ -11,7 +11,7 @@ const WhatGPT3 = () => {
       <div className='gpt3__whatgpt3-heading'>
         <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
         <p>Explore The Library</p>
-      </div>
+      </div> 
       <div className="gpt3__whatgpt3-container">
         <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "/>
         <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"/>
